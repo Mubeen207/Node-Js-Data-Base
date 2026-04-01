a83b17f23d068a9054b62330c04c54df2160cf31
mubeen207 <mubeenulislam207@gmail.com>
2026-04-01 23:41:58 +0500

    Facing Error

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const todo = [];
+
+app.get("/", (req, res) => {
+  res.send({ message: "Home Page" });
+});
+
+app.get("/todos", (req, res) => {
+  res.send(todo);
+});
+
+app.post("/todo", (req, res) => {
+  const todo = req.body;
+  todo.push({ ...todo, id: todo.lenght + 1 });
+  res.send("added Sussecfully");
+});
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
