f783b50126406988d85dc6ded3ff678afac7c902
mubeen207 <mubeenulislam207@gmail.com>
2026-04-02 00:05:28 +0500

    post get delete complete

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import express from "express";
-
 const app = express();
 app.use(express.json());
 
-const todo = [];
+let todo = [];
 
 app.get("/", (req, res) => {
   res.send({ message: "Home Page" });
@@ -15,10 +14,30 @@ app.get("/todos", (req, res) => {
 
 app.post("/todo", (req, res) => {
   const getTodo = req.body;
-  todo.push({ ...getTodo, id: todo.length + 1});
+  todo.push({ ...getTodo, id: generateID(8) });
   res.send("added Sussecfully");
+});
+
+app.delete("/todo/:id", (req, res) => {
+  const { id } = req.params;
+  todo = todo.filter((todo) => todo.id !== id);
+  console.log(todo, id);
+
+  res.send("Deleted Sussecfully");
 });
 
 app.listen(3000, () => {
   console.log("Server Started");
 });
+
+export default function generateID(length = 10) {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return result;
+}
