a802651b9281827a5db80f0ab4597496db78ad81
mubeen207 <mubeenulislam207@gmail.com>
2026-04-11 20:02:04 +0500

    .next deleted

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qvCFVQcMj2geKW2MJ2maFrZW7X1PPtKwAJpnjFz/wIE=\"\n}"
--- frontend/.next/dev/static/chunks/src_app_page_09~7opn.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_072m3rb._.js"],
-    source: "dynamic"
-});
